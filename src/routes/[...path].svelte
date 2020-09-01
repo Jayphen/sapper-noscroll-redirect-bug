1862a50f6301f96de49287594723c9fd884e9663
Jayphen <software@jayphen.com>
2020-09-01 15:12:13 +0200

    Init

---
+++ src/routes/[...path].svelte
@@ -0,0 +1,10 @@
+<script context="module">
+  import { goto } from "@sapper/app";
+  export async function preload({ path }, session) {
+    if ("/blah".includes(path)) {
+      this.redirect(301, "redirect-to");
+    }
+  }
+</script>
+
+nothing
---
+++ src/routes/about.svelte
@@ -0,0 +1,17 @@
+<style>
+  .spacer {
+    height: 100em;
+  }
+</style>
+
+<svelte:head>
+  <title>About</title>
+</svelte:head>
+
+<h1>About this site</h1>
+
+<p>This is the 'about' page. There's not much here.</p>
+
+<div class="spacer" />
+
+<a href="/blah">Trigger a redirect</a>
